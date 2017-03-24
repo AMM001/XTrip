e0204cdda5c83975c6b0433e6824543efe235d03
merna <merna108@gmail.com>
2017-03-24 10:35:06 +0200

    Reminder

---
+++ app/src/main/java/com/fx/merna/xtrip/views/activities/ReminderDialogActivity.java
@@ -0,0 +1,18 @@
+package com.fx.merna.xtrip.views.activities;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.fx.merna.xtrip.R;
+
+public class ReminderDialogActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminder);
+    }
+}
